008770f6710cb62bab66e11c9b742cb69d1fe63c
Codeurge123 <bansalyash316@gmail.com>
2025-07-16 08:15:54 +0530

    github data added

--- index.js
+++ index.js
@@ -7,6 +7,42 @@ const app = express();
 
 const port = 3000; // ye hamra virtual port hai.
 
+const githubData = {
+  "login": "codeurge123",
+  "id": 174909202,
+  "node_id": "U_kgDOCmznEg",
+  "avatar_url": "https://avatars.githubusercontent.com/u/174909202?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/codeurge123",
+  "html_url": "https://github.com/codeurge123",
+  "followers_url": "https://api.github.com/users/codeurge123/followers",
+  "following_url": "https://api.github.com/users/codeurge123/following{/other_user}",
+  "gists_url": "https://api.github.com/users/codeurge123/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/codeurge123/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/codeurge123/subscriptions",
+  "organizations_url": "https://api.github.com/users/codeurge123/orgs",
+  "repos_url": "https://api.github.com/users/codeurge123/repos",
+  "events_url": "https://api.github.com/users/codeurge123/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/codeurge123/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "code_urge",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 15,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 1,
+  "created_at": "2024-07-06T21:42:55Z",
+  "updated_at": "2025-05-19T15:49:34Z"
+}
+
 // Now Get Request kar lo
 // Syntax :
 // app.get('url' , callback);
@@ -26,6 +62,10 @@ app.get("/login", (req, res) => {
 
 app.get('/youtube' , (req,res) => {
     res.send('<h2>Chai aur code</h2>')
+})
+
+app.get('/github', (req,res)  => {
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
